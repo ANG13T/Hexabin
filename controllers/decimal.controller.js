a99dcf8a4df5995183c798b4ceb0b9538b767130
angelina-tsuboi <angelina.t1832@gmail.com>
2020-11-17 06:16:00 -0800

    hex controller changes

--- controllers/decimal.controller.js
+++ controllers/decimal.controller.js
@@ -1,37 +1,3 @@
-function getHexNumber(length){
-    return '#'+Math.floor(16**5 + Math.random()*(16**6-16**5)).toString(16);;
-}
-
-function isValidHex(hex) {
-    var a = parseInt(hex,16);
-    return (a.toString(16) === h)
-}
-
-function isValidConversion(conversion){
-    if(conversion == "binary" || conversion == "octal" || conversion == "decimal"){
-        return true;
-    }
-    return false
-}
-
-function convertHex(hex, conversion){
-    let binary = (parseInt(hex, 16).toString(2)).padStart(8, '0');
-
-    if(conversion == "octal"){
-        let octal = parseInt(binary, 2).toString(8);
-        return octal;
-    }
-
-    if(conversion == "binary"){
-        return binary;
-    }
-
-    if(conversion == "decimal"){
-        let digit = parseInt(hex, 16);
-        return digit
-    }
-}
-
 
 //Get multiple decimal numbers
 exports.getDecimalNumbers = function(req, res) {
--- controllers/hex.controller.js
+++ controllers/hex.controller.js
@@ -1,3 +1,38 @@
+function getHexNumber(length){
+    return '#'+Math.floor(16**5 + Math.random()*(16**6-16**5)).toString(16);;
+}
+
+function isValidHex(hex) {
+    var a = parseInt(hex,16);
+    return (a.toString(16) === h)
+}
+
+function isValidConversion(conversion){
+    if(conversion == "binary" || conversion == "octal" || conversion == "decimal"){
+        return true;
+    }
+    return false
+}
+
+function convertHex(hex, conversion){
+    let binary = (parseInt(hex, 16).toString(2)).padStart(8, '0');
+
+    if(conversion == "octal"){
+        let octal = parseInt(binary, 2).toString(8);
+        return octal;
+    }
+
+    if(conversion == "binary"){
+        return binary;
+    }
+
+    if(conversion == "decimal"){
+        let digit = parseInt(hex, 16);
+        return digit
+    }
+}
+
+
 //Get a HEX number
 exports.getHexNumber = function(req, res) {
     res.send('NOT IMPLEMENTED: Get a HEX number');
