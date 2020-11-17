aead4764ee5a12d83166513dd2a9330d6de909c9
angelina-tsuboi <angelina.t1832@gmail.com>
2020-11-17 06:35:03 -0800

    wrote helpers for octal

--- controllers/octal.controller.js
+++ controllers/octal.controller.js
@@ -1,3 +1,23 @@
+function getOctalNumber(length){
+    let octal = ""
+    for(let i = 0; i < length; i++){
+      octal += (Math.random() * 8)  
+    }
+    return octal
+}
+
+function isValidOctal(octal) {
+   
+}
+
+function isValidConversion(conversion){
+    if(conversion == "binary" || conversion == "decimal" || conversion == "hex"){
+        return true;
+    }
+    return false
+}
+
+
 //Get a octal number
 exports.getOctalNumber = function(req, res) {
     res.send('NOT IMPLEMENTED: Get a binary number');
