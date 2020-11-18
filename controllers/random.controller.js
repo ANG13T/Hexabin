ea32e42b6bc985dc22a19f60093c63a0e8f563bc
angelina-tsuboi <angelina.t1832@gmail.com>
2020-11-18 07:55:44 -0800

    converted random controller

--- controllers/random.controller.js
+++ controllers/random.controller.js
@@ -39,26 +39,40 @@ function isValidBase(base){
     return (base == "hex" || base == "binary" || hex == "decimal" || base == "octal");
 }
 
-//Get a random number of any base
-exports.getRandomNumber = function(req, res) {
-    // res.send('NOT IMPLEMENTED: Get a random number of any base');
-    let result = {error: false, result: []}
-    let base = generateRandomBase();
-    let length = Math.floor(Math.random() * 6) + 1;
-
-    result.result = generateRandomNumber(base, length)
-    res.send(result)
-};
-
 //Get a random numbers of any base
 exports.getRandomNumbers = function(req, res) {
     let result = {result: [], error: false}
     let amount;
-    if(req.params.amount){
-        amount = parseInt(req.params.amount)
+    let length;
+    let base;
+
+    if(req.query.length){
+        length = parseInt(req.query.length)
+
+        if(!length){
+            result.error = `Invalid argument for length in path: ${req.query.length}`
+            res.send(result)
+            return;
+        }
+
+    }
+
+    if(req.query.base){
+        base = req.query.base;
+
+        if(!isValidBase(base)){
+            result.error = `Invalid argument for base in path: ${base}`
+            res.send(result)
+            return;
+        }
+
+    }
+
+    if(req.query.amount){
+        amount = parseInt(req.query.amount)
 
         if(!amount){
-            result.error = `Invalid argument in path: ${req.params.amount}`
+            result.error = `Invalid argument in path: ${req.query.amount}`
             res.send(result)
             return;
         }
@@ -74,86 +88,11 @@ exports.getRandomNumbers = function(req, res) {
 
     let randomList = []
     for(let i = 0; i < amount; i++){
-        let base = generateRandomBase();
-        let length = Math.floor(Math.random() * 6) + 1;
-        randomList.push(generateRandomNumber(base, length))
+        let base = base ? base : generateRandomBase();
+        let randomLength = length ? length : Math.floor(Math.random() * 6) + 1;
+        randomList.push(generateRandomNumber(base, randomLength))
     }
     result.result = randomList;
 
     res.send(result)
-};
-
-//Get a random number of a specific base
-exports.getRandomNumberOfBase = function(req, res) {
-    let result = {result: [], error: false}
-    let base;
-    if(req.params.base){
-        base = req.params.base;
-
-        if(!isValidBase(base)){
-            result.error = `Invalid argument for base in path: ${base}`
-            res.send(result)
-            return;
-        }
-
-        let length = Math.floor(Math.random() * 6) + 1;
-        result.result = generateRandomNumber(base, length)
-        res.send(result);
-        return;
-
-    }else{
-        result.error = "Argument for base of path is undefined"
-        res.send(result)
-        return;
-    }
-};
-
-//Get a random numbers of a specific base
-exports.getRandomNumbersOfBase = function(req, res) {
-    let result = {result: [], error: false}
-    let amount;
-    let base;
-    if(req.params.amount){
-        amount = parseInt(req.params.amount)
-
-        if(!amount){
-            result.error = `Invalid argument in path: ${req.params.amount}`
-            res.send(result)
-            return;
-        }
-
-        if(amount > 20){
-            result.error = "MAX of 20 items"
-            res.send(result)
-            return;
-        }
-    }else{
-        amount = 1
-    }
-
-    if(req.params.base){
-        base = req.params.base;
-
-        if(!isValidBase(base)){
-            result.error = `Invalid argument for base in path: ${base}`
-            res.send(result)
-            return;
-        }
-        
-        let values = []
-
-        for(let i = 0; i < amount; i++){
-            let length = Math.floor(Math.random() * 6) + 1;
-            values.push(generateRandomNumber(base, length))
-        }
-
-        result.result = values;
-        res.send(result);
-        return;
-
-    }else{
-        result.error = "Argument for base of path is undefined"
-        res.send(result)
-        return;
-    }
 };
--- routes/random.route.js
+++ routes/random.route.js
@@ -5,13 +5,6 @@ var RandomController = require('../controllers/random.controller')
 
 // GETTING
 
-router.get('/random', RandomController.getRandomNumber)
-
-router.get('/random/:amount', RandomController.getRandomNumbers)
-
-router.get('/random/base/:base', RandomController.getRandomNumberOfBase)
-
-router.get('/random/base/:base/:amount', RandomController.getRandomNumbersOfBase)
-
+router.get('/random', RandomController.getRandomNumbers)
 
 module.exports = router;
